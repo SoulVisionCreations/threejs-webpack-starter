ad8c970c37fac0174521e5953eb843df8fc312ea
SumeetSD-avataar <sumeetsouravdas@avataar.me>
2022-02-16 01:07:11 +0530

    1st commit

--- src/script.js
+++ src/script.js
@@ -2,104 +2,67 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 
-// Debug
-const gui = new dat.GUI()
+let url = new URL(window.location.href);
+let c = url.searchParams.get("c");
+console.log("params",c)
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
-
-// Scene
+const canvas = document.querySelector('.webgl')
 const scene = new THREE.Scene()
 
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const loader = new GLTFLoader()
 
-// Materials
+function loading(i){
+    loader.load('https://d77dh4faztz32.cloudfront.net/pepperfry/skuAssets/1599490/scene.glb', function(glb){
+        console.log(glb)
+        const root = glb.scene;
+        root.position.set(i,0,0)
+        root.scale.set(1,1,1)
+        scene.add(root);
+    }, function(xhr){
+        console.log((xhr.loaded/xhr.total*100) + " % loaded")
+    },
+    function(error){
+        console.log("error")
+    })
+}
+// loading(0);
+for(let i = 0; i <c; i+=1)
+{
+    loading(i);
+}
+const light = new THREE.DirectionalLight(0xffffff,1)
+light.position.set(2,2,5)
+scene.add(light)
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
 
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+// const geometry = new THREE.BoxGeometry(1,1,1)
+// const material = new THREE.MeshBasicMaterial({color : 0x00ff00 })
+// const boxMesh = new THREE.Mesh(geometry,material)
+// scene.add(boxMesh)
 
-// Lights
-
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
-
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+//hap setup
+const sizes ={
+    width : window.innerWidth,
+    height : window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height , 0.1, 100)
+camera.position.set(0,1,2)
 scene.add(camera)
 
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+const renderer = new THREE.WebGL1Renderer({
+    canvas : canvas
 })
+
 renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+renderer.updateShadowMap.enabled = true
+renderer.gammaOutput = true
 
-/**
- * Animate
- */
-
-const clock = new THREE.Clock()
-
-const tick = () =>
-{
-
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
-
-    // Update Orbital Controls
-    // controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+function animate(){
+    requestAnimationFrame(animate)
+    renderer.render(scene,camera)
 }
-
-tick()
+animate()
